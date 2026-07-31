1220c05573f59fae9dad8693aa56f6bd25f50749
Edgar Guerrero <edgarguerrero37@gmail.com>
2026-07-31 12:05:17 -0500

    Dashboard y Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import './App.css';
 // import Footer from './components/Footer';
 // import Navbar from './components/Navbar';
 // import Hero from './components/Hero';
-import Servicios from './pages/Servicios';
+//import Servicios from './pages/Servicios';
+import DashboardLayout from './layouts/DashboardLayout';
+import {Routes, Route} from 'react-router-dom';
+import PanelGeneral from './pages/PanelGeneral';
+import Productos from './pages/Productos';
+import NotFound from './pages/NotFound';
 
 function App(){
   return(
-    <>
-      <Servicios />
-    </>
+    <Routes>
+      <Route path="/" element={<DashboardLayout/>}>
+        <Route index element={<PanelGeneral />}/>
+        <Route path="productos" element={<Productos/>}/>
+      </Route>
+
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
   );
 };
 
