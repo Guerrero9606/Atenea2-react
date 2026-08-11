f7f4d0ed14607dcdaaef2c7125b4952a83ebb09d
Edgar Guerrero <edgarguerrero37@gmail.com>
2026-08-11 11:51:20 -0500

    Creacion de Hooks, useState y props

--- src/pages/PanelGeneral.jsx
+++ src/pages/PanelGeneral.jsx
@@ -1,8 +1,12 @@
+import ContadorVentas from "../components/ContadorVentas";
+
 export default function PanelGeneral(){
     return(
         <div>
             <h1>Panel General</h1>
             <p>Bienvenido al sistema.</p>
+
+            <ContadorVentas />
         </div>
     );
 }
